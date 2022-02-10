3e9bb6329dd1e6e6a64c371a4413e810ee53aa86
michael <4apaev@gmail.com>
2022-02-10 07:09:49 +0200

    add trace

--- util/fail.js
+++ util/fail.js
@@ -18,6 +18,29 @@ export default class Fail extends Error {
   static assert = (a, b) => a || this.raise(b, { cause: 'assert' })
   static raise = (m, c) => { throw this.as(m, c) }
   static deny = (m, c) => Promise.reject(this.as(m, c))
+  static trace(start = Fail.trace) {
+    const ps = Error.prepareStackTrace
+    Error.prepareStackTrace = (_, s) => s
+
+    const e = new Error
+    Error.captureStackTrace(e, start)
+
+    const { stack } = e
+    Error.prepareStackTrace = ps
+
+    return stack.map(cs => ({
+      file: cs.getFileName(),
+      row: cs.getLineNumber(),
+      col: cs.getColumnNumber(),
+      fname: cs.getFunctionName(),
+      // self    : cs.getThis(),
+      // method  : cs.getMethodName(),
+      // type    : cs.getTypeName(),
+      // isCtor  : cs.isConstructor(),
+      // isAsync : cs.isAsync(),
+      // isNative: cs.isNative(),
+    }))
+  }
 }
 
 export const deny = Fail.deny
