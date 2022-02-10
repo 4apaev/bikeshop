194fa61fed475350a79b06a4bb33383e74f2b885
michael <4apaev@gmail.com>
2022-02-10 15:12:16 +0200

    typings

--- wheels/middleware.js
+++ wheels/middleware.js
@@ -1,11 +1,8 @@
 // @ts-check
-import * as Mim from './mim.js'
+import Koa from 'koa'
+import * as Mim from '../util/mim.js'
 import { Log } from '../util/index.js'
 
-/** @typedef {import("koa").Context} Ctx */
-/** @typedef {import("koa").Middleware} MWare */
-
-// @ts-ignore
 const debug = Log.debug('middleware')
 
 export const methods = new Set([
@@ -15,14 +12,14 @@ export const methods = new Set([
   'DELETE',
 ])
 
-/** @type {MWare} */
+/** @type {Koa.Middleware} */
 export async function logger(ctx, next) {
   const start = Date.now()
   await next()
   Log('%d %s %s %s', ctx.status, ctx.method, ctx.path, Date.now() - start)
 }
 
-/** @type {MWare} */
+/** @type {Koa.Middleware} */
 export async function reqPayload(ctx, next) {
   if (!methods.has(ctx.method))
     return next()
@@ -36,17 +33,15 @@ export async function reqPayload(ctx, next) {
   return parsePayload(ctx, next)
 }
 
-/** @type {MWare} */
+/** @type {Koa.Middleware} */
 function parsePayload(ctx, next) {
   if (!ctx.is(Mim.json))
     return next()
 
   if (ctx.payload.length < 2) {
-    // @ts-ignore
-    return next(ctx.payload = {}, debug(
-      '[payload: to small] %s:',
-      ctx.req.headers[ 'content-length' ],
-      ctx.payload))
+    ctx.payload = {}
+    debug('payload to small %s:', ctx.req.headers[ 'content-length' ])
+    return next()
   }
 
   try {
