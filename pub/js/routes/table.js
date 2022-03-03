ac115c8624dba594b61791208462cf9553423b89
michael <4apaev@gmail.com>
2022-03-03 06:07:43 +0200

    dunno

--- pub/js/routes/table.js
+++ pub/js/routes/table.js
@@ -6,62 +6,64 @@ import _Form from '/js/comp/form.js'
 const µ = undefined
 
 export default class Table extends Base {
-
-  slug = 'table'
-  title = 'table'
-  apiUrl = `/api/${ this.slug }`
-
-  css = `@import url('/css/table.styl?uid=${ this.uid }')`
-  cells = []
-  fields = []
-  tmpl = `
-    <header>
-      <h2 class="title">${ this.title }</h2>
-      <button class="btn ok create">create</button>
-    </header>
-
-    <table>
-    </table>
-
-    <dialog>
-      <button class="close">✖</button>
-
-      <ws-form
-        class="create-entry"
-        title="${ this.title }"
-        apiurl="${ this.apiUrl }"
-      >
-      </ws-form>
-    </dialog>
-  `
-
+  #req
   #form
   #dialog
 
+  css = `@import url('/css/table.styl?uid=${ this.uid }')`
+
+  cells = []
+  fields = []
+
+  get tmpl() {
+    const ttl = this.title ??= this.get('title')
+    const api = this.apiurl ??= this.get('apiurl')
+    return `
+      <header>
+        <h2 class="title">${ ttl }</h2>
+        <button class="btn ok create">create</button>
+      </header>
+
+      <table>
+      </table>
+
+      <dialog>
+        <button class="close">✖</button>
+
+        <ws-form
+          class="create-entry"
+          title="${ ttl }"
+          apiurl="${ api }"
+        >
+        </ws-form>
+      </dialog>
+    `
+  }
+
   get form() {
-    return this.#form ??= this.$('ws-form') 
+    return this.#form ??= this.$('ws-form')
   }
 
   get dialog() {
-    return this.#dialog ??= this.$('dialog') 
+    return this.#dialog ??= this.$('dialog')
   }
 
   connectedCallback() {
     super.connectedCallback()
+
     this.on('click', '.btn.create', this.toggleDialog)
     this.on('click', 'button.close', (e, stop) => (this.dialog.close(), stop))
 
-    Sync.get(this.apiUrl, { limit: 10 })
-        .then(this.render)
-        .catch(Log.error)
-
+    this.apiurl ??= this.get('apiurl')
+    Sync.get(this.apiurl, { limit: 10 })
+        .then(this.render, Log.error)
   }
 
   toggleDialog = (e, stop) => {
     if (this.dialog.has('open'))
-      return this.dialog.close()
-      // , history.back(), stop
-    this.dialog.showModal()
+      this.dialog.close()
+    else
+      this.dialog.showModal()
     return stop
   }
 
@@ -69,7 +71,7 @@ export default class Table extends Base {
     const { frag } = $
     const form = this.$('ws-form')
 
-    for (const row of body.value) {
+    for (const row of body) {
       const tr = $.tr()
       for (const x of this.cells)
         tr.append($.td(µ, row[ x.key ]))
