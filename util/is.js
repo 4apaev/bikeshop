7bb2d9cb47c3fd80950294550dd6344a5f304e22
michael <4apaev@gmail.com>
2022-05-08 03:46:17 +0300

    typings

--- util/is.js
+++ util/is.js
@@ -11,35 +11,22 @@ export const {
 export { Fail }
 
 /**
- * @typedef {(x: any, m?: string) => boolean} TypeIs */
-
-/**
- * @typedef {(a: any, m?: string) => boolean} TIs */
-
-/**
- * @typedef {(a: any, b: any, m?: string) => boolean} TwoTypeIs */
-
-/**
- * <T>
- * param {*} [a]
- * param {*} [b]
- * param {string} [m]
- * return {is typeof T}
- */
+ * @typedef {(x: any, m?: string) => boolean} TypeIs             *//**
+ * @typedef {(a: any, m?: string) => boolean} TIs                *//**
+ * @typedef {(a: any, b: any, m?: string) => boolean} TwoTypeIs  */
 
 /**
  * @parse
- * @template T
+ * @template {function} T
  * @param {T} a
  * @param {*=} b
  * @param {string=} m
  * @return {b is typeof T}
  */
 export default function Is(a, b, m) {
-  let c = T(b)
-  return It(m, 'instance', typeof a == 'function'
-    ? c == a.name || a[ Symbol.hasInstance ](b)
-    : c == a)
+  return arguments.length < 2
+    ? a != undefined
+    : It(m, 'instance', a?.name == T(b) || a[ Symbol.hasInstance ](b))
 }
 
 /**
@@ -58,11 +45,19 @@ export function use(name, fn) {
     Is[ a ] = Is[ k ]
 }
 
+/**
+ * @param  {*} x
+ * @return {string}
+ */
 export function T(x) {
   return toString.call(x).slice(8, -1)
 }
 
-/** @type {import('./utils.js').IT} */
+/**
+ * @param {string} m
+ * @param {string} name
+ * @param {boolean} x
+ */
 function It(m, name, x) {
   const { not, fail } = It
   It.not = It.fail = false
@@ -101,20 +96,20 @@ Object.defineProperties(Is, Object.getOwnPropertyDescriptors({
    * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
   * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
  * * */
-
-use('integer  i int'    , Is.i = /** @type {TypeIs} */ (x, _) => Number.isInteger(x))
-use('number   n num'    , Is.n = /** @type {TypeIs} */ (x, _) => Number.isFinite(x))
-use('array    a arr'    , Is.a = /** @type {TypeIs} */ (x, _) => Array.isArray(x))
-use('string   s str'    , Is.s = /** @type {TypeIs} */ (x, _) => typeof x == 'string')
-use('boolean  b bool'   , Is.b = /** @type {TypeIs} */ (x, _) => typeof x == 'boolean')
-use('function f fn'     , Is.f = /** @type {TypeIs} */ (x, _) => typeof x == 'function')
-use('object   o obj'    , Is.o = /** @type {TypeIs} */ (x, _) => typeof x == 'object' && !!x)
-use('Error    e err'    , Is.e = /** @type {TypeIs} */ (x, _) => Error[ Symbol.hasInstance ](x))
-use('Object   O Obj'    , Is.O = /** @type {TypeIs} */ (x, _) => T(x) == 'Object')
-use('complex  x cmplx'  , Is.x = /** @type {TypeIs} */ (x, _) => x === Object(x))
-use('iterable I itr'    , Is.I = /** @type {TypeIs} */ (x, _) => Symbol.iterator in Object(x))
-use('ok'                , x =>   /** @type {TypeIs} */ (x, _) != null)
-use('template tmpl'     , x =>   /** @type {TypeIs} */ Array.isArray(x?.raw))
+use('integer  i int'    , Is.i =  /** @type {TypeIs} */ x => Number.isInteger(x))
+use('number   n num'    , Is.n =  /** @type {TypeIs} */ x => Number.isFinite(x))
+use('array    a arr'    , Is.a =  /** @type {TypeIs} */ x => Array.isArray(x))
+use('string   s str'    , Is.s =  /** @type {TypeIs} */ x => typeof x == 'string')
+use('boolean  b bool'   , Is.b =  /** @type {TypeIs} */ x => typeof x == 'boolean')
+use('function f fn'     , Is.f =  /** @type {TypeIs} */ x => typeof x == 'function')
+use('object   o obj'    , Is.o =  /** @type {TypeIs} */ x => typeof x == 'object' && !!x)
+use('Error    e err'    , Is.e =  /** @type {TypeIs} */ x => Error[ Symbol.hasInstance ](x))
+use('RegExp   r rx'     , Is.rx = /** @type {TypeIs} */ x => RegExp[ Symbol.hasInstance ](x))
+use('Object   O Obj'    , Is.O =  /** @type {TypeIs} */ x => T(x) == 'Object')
+use('complex  x cmplx'  , Is.x =  /** @type {TypeIs} */ x => x === Object(x))
+use('iterable I itr'    , Is.I =  /** @type {TypeIs} */ x => Symbol.iterator in Object(x))
+use('ok'                , Is.ok = /** @type {TypeIs} */ x => x != null)
+use('template tmpl'     , Is.tmpl = /** @type {TypeIs} */ x => Array.isArray(x?.raw))
 use('equal    eq eql'   , function equal(a, b, t) {
   if (a === b)                  return true
   if ((t = T(a)) != T(b))       return false
